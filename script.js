296a827e2dba3a9374ca4cdcb2937bef9c30ffdc
IagoCustodio <iagoo.custodio@gmail.com>
2024-02-24 15:15:12 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,73 @@
+// Get tabLinks & tabContents
+var tabLinks = document.getElementsByClassName("tab-links"),
+  tabContents = document.getElementsByClassName("tab-contents");
+
+// On openTab fucntion call
+function openTab(tabname) {
+  for (item of tabLinks) {
+    item.classList.remove("active-link");
+  }
+  for (item of tabContents) {
+    item.classList.remove("active-tab");
+  }
+  event.currentTarget.classList.add("active-link");
+  document.getElementById(tabname).classList.add("active-tab");
+}
+
+// Get SideMenu
+var sideMenu = document.getElementById("sidemenu");
+
+// On openTab fucntion call
+function openMenu() {
+  sideMenu.style.right = "0";
+}
+
+function closeMenu() {
+  sideMenu.style.right = "-200px";
+}
+
+
+/*=============== SKILLS TABS ===============*/
+
+const tabs = document.querySelectorAll('[data-target]'),
+      tabContent = document.querySelectorAll('[data-content]')
+
+      tabs.forEach(tab => {
+        tab.addEventListener("click", () => {
+            const target = document.querySelector(tab.dataset.target)
+
+            tabContent.forEach(tabContents => {
+                tabContents.classList.remove('skills_active')
+            })
+
+            target.classList.add('skills_active')
+            
+
+            tabs.forEach(tab => {
+                tab.classList.remove('skills_active')
+            })
+
+            tab.classList.add('skills_active')
+        }) 
+      }) 
+
+/*=============== MIXITUP FILTER PORTFOLIO ===============*/
+
+let mixerPortfolio = mixitup('.work_container', {
+    selectors: {
+        target: '.work_card'
+    },
+    animation: {
+        duration: 300
+    }
+});
+
+
+
+
+
+
+
+
+
+
